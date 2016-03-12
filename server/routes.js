f17203cdf7ee90b24f845f946006a5945c65fbcf
chououtside <chouming3@gmail.com>
2016-03-12 14:46:21 -0800

    Remove Comments

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,4 @@ var router = require('express').Router();
 var boardController = require('./board/boardController');
 var gymLeader = require('./gymLeader/gymLeaderController.js');
 
-//router.get('/pokemon/:pokemonID', pokemonController.get);
-//router.get('/gymleader/:gymleaderID', gymLeaderController.get);
-
 module.exports = router;
