95e08cd881e514220726add3ae38c801ce62f36d
chououtside <chouming3@gmail.com>
2016-03-12 14:54:14 -0800

    Remove board controller variable

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,4 @@
 var router = require('express').Router();
-var boardController = require('./board/boardController');
 var gymLeader = require('./gymLeader/gymLeaderController.js');
 
 module.exports = router;
