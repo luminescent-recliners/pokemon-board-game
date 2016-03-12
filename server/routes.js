b4f60e19b86717022af0d6ff9c2bcfb690e0148c
Arthi <aarti156@gmail.com>
2016-03-11 16:38:19 -0800

    Adds Schema for gymLeader Runs the express server new file:   server/gymLeader/gymLeaderController.js new file:   server/gymLeader/gymLeaderModel.js modified:   server/routes.js modified:   server/server.js

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,8 @@
 var router = require('express').Router();
 var boardController = require('./board/boardController');
+var gymLeader = require('./gymLeader/gymLeaderController.js');
 
-router.get('/pokemon/:pokemonID', pokemonController.get);
-router.get('/gymleader/:gymleaderID', gymLeaderController.get);
+//router.get('/pokemon/:pokemonID', pokemonController.get);
+//router.get('/gymleader/:gymleaderID', gymLeaderController.get);
 
 module.exports = router;
