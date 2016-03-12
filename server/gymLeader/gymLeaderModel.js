b3ace4b7515f003d81260307a900e0e187f52e92
Arthi <aarti156@gmail.com>
2016-03-11 19:06:26 -0800

    Refactors gymLeader Schema modified:   server/gymLeader/gymLeaderModel.js

--- server/gymLeader/gymLeaderModel.js
+++ server/gymLeader/gymLeaderModel.js
@@ -4,14 +4,7 @@ var gymLeaderSchema = new mongoose.Schema({
   name: String,
   city: String,
   attackStrength: Number,
-  attackBonus: {
-    1: Number,
-    2: Number,
-    3: Number,
-    4: Number, 
-    5: Number, 
-    6: Number
-  },
+  attackBonus: {},
   imageURL: String
 });
 
