6f6a0dfae66ae78a9b3169d13829c885d482eab0
chououtside <chouming3@gmail.com>
2016-03-12 15:54:32 -0800

    Move app.use(router)
    Move app.use(router) below app.use(parser) to be able to parse requests and retrieve body
    
    	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -7,12 +7,12 @@ mongoose.connect('mongodb://localhost/testDB');
 
 var router = require('./routes.js');
 var app = express();
-app.use(router);
 
 var port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+app.use(router);
 app.use(express.static(__dirname + '/../public'));
 
 app.listen(port);
