48bea72a26003f1fd4e13b0fdd88894bf2224552
Linda Zou <linda.ye.zou@gmail.com>
2016-03-12 16:52:26 -0800

    Adds 2 gym leaders
    Adds two values to the gym leader data, Brock and Misty
    
     Changes to be committed:
    	modified:   data/gymLeaderData.js

---
+++ server/data/gymLeaderData.js
@@ -0,0 +1,30 @@
+{
+  '1': {
+	name: 'Brock/Onyx',
+  	city: 'Pewter',
+  	attackStrength: 4,
+  	attackBonus: {
+  		'1': 1,
+  		'2': 2,
+  		'3': 3,
+  		'4': 2,
+  		'5': 3,
+  		'6': 1
+  	},
+  	imageURL: "String"
+  },
+  '2': {
+	name: 'Misty/Starmie',
+  	city: 'Cerulean',
+  	attackStrength: 4,
+  	attackBonus: {
+  		'1': 2,
+  		'2': 3,
+  		'3': 4,
+  		'4': 3,
+  		'5': 4,
+  		'6': 2
+  	},
+  	imageURL: "String"
+  }
+}
