e05cc4772ddf6775bc8788d180352e327abefab8
Linda Zou <lindazou@Lindas-MacBook-Pro.local>
2016-03-12 16:56:32 -0800

    Adds pokemon data
    Adds the data for the 6 starter pokemon: Squirtle, Charmander, Bulbasaur, Pikachu, Clefairy, Meowth
    
     Changes to be committed:
    	modified:   data/pokemonData.js

---
+++ server/data/pokemonData.js
@@ -0,0 +1,116 @@
+{
+  '1': {
+    name: 'Bulbasaur',
+    pokemonID: 1,
+    color: 'pink',
+    description: 'For some time after its birth, it grows by gaining nourishment from the seed on its back.',
+    visible: {
+      type: Boolean,
+      default: true
+    },
+    alive: {
+      type: Boolean,
+      default: true
+    },
+    imageURL: '/api/v1/pokemon/1/',
+    specs: {
+      attackName: 'Leech Seed',
+      attachStrength: 4
+    }
+  },
+  '4': {
+    name: 'Charmander',
+    pokemonID: 4,
+    color: 'pink',
+    description: 'From the time it is born, a flame burns at the tip of its tail. Its life would end if the flame were to go out.',
+    visible: {
+      type: Boolean,
+      default: true
+    },
+    alive: {
+      type: Boolean,
+      default: true
+    },
+    imageURL: '/api/v1/pokemon/4/',
+    specs: {
+      attackName: 'Ember',
+      attachStrength: 4
+    }
+  },
+  '7': {
+    name: 'Squirtle',
+    pokemonID: 7,
+    color: 'pink',
+    description: 'It shelters itself in its shell, then strikes back with spouts of water at every opportunity.',
+    visible: {
+      type: Boolean,
+      default: true
+    },
+    alive: {
+      type: Boolean,
+      default: true
+    },
+    imageURL: '/api/v1/pokemon/7/',
+    specs: {
+      attackName: 'Water Gun',
+      attachStrength: 4
+    }
+  },
+  '25': {
+    name: 'Pikachu',
+    pokemonID: 25,
+    color: 'pink',
+    description: 'It occasionally uses an electric shock to recharge a fellow Pikachu that is in a weakened state.',
+    visible: {
+      type: Boolean,
+      default: true
+    },
+    alive: {
+      type: Boolean,
+      default: true
+    },
+    imageURL: '/api/v1/pokemon/25/',
+    specs: {
+      attackName: 'ThunderShock',
+      attachStrength: 3
+    }
+  },
+  '35': {
+    name: 'Clefairy',
+    pokemonID: 35,
+    color: 'pink',
+    description: 'The moonlight that it stores in the wings on its back apparently gives it the ability to float in midair.',
+    visible: {
+      type: Boolean,
+      default: true
+    },
+    alive: {
+      type: Boolean,
+      default: true
+    },
+    imageURL: '/api/v1/pokemon/35/',
+    specs: {
+      attackName: 'Pound',
+      attachStrength: 3
+    }
+  },
+  '52': {
+    name: 'Meowth',
+    pokemonID: 52,
+    color: 'pink',
+    description: 'MEOWTH withdraws its sharp claws into its paws to slinkily sneak about without making any incriminating footsteps. For some reason, this POKMON loves shiny coins that glitter with light.',
+    visible: {
+      type: Boolean,
+      default: true
+    },
+    alive: {
+      type: Boolean,
+      default: true
+    },
+    imageURL: '/api/v1/pokemon/52/',
+    specs: {
+      attackName: 'Bite',
+      attachStrength: 3
+    }
+  }
+}
