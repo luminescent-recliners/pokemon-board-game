20744c50e50b467a7ab288dd899a7662706f69aa
Linda Zou <lindazou@Lindas-MacBook-Pro.local>
2016-03-14 14:50:12 -0700

    Implements home view
    Home view features game name input to compile a user game list and a available game list that links to respective lobby.
    
     Changes to be committed:
    	modified:   public/home/home.html
    	modified:   public/home/home.js
    	modified:   public/index.html
    	modified:   public/services.js

--- public/home/home.js
+++ public/home/home.js
@@ -1,4 +1,37 @@
 angular.module('pokemon.home', [])
-.controller('pokemonController',function($scope) {
-  $scope.test = "I GOT HERE";
+
+.controller('homeController',function($scope) {
+  $scope.user =  
+  	{ 
+	facebookId: 'Facebook123',
+	displayName: 'Henry',
+	gamesParticipating: [1],
+	numGameWon: 0
+	}
+  $scope.gameId = 0;
+  $scope.gameList = [];
+
+  $scope.hitEnter = function($event, input) {
+    if($event.which === 13) {
+    $scope.makeNewGame(input);
+  	}
+  };
+
+  $scope.makeNewGame = function(newGameName) {
+  	$scope.gameList.push($scope.newGameName);
+  	userFactory.addGame($scope.newGameName, user.facebookId)
+  	.then(function (resp) {
+      $location.path('/board');
+    }).catch(function (error) {
+      console.error(error);
+    });
+  	$scope.newGameName = '';
+  	$scope.gameId++;
+  	}
+  }
 });
+// window.localStorage.setItem('gameId', $scope.gameId);
+// if(window.localStorage.getItem('gameId') === null) {
+// 	console.log('null');
+
+// id and game name, game creator 
