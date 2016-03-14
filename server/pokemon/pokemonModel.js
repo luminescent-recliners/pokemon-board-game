4d83dc8bc93198fca3db6bdd480c3cf20147d1b3
Linda Zou <linda.ye.zou@gmail.com>
2016-03-14 11:31:57 -0700

    Refactors schemas
    Changes the schemas to accommodate importing individual objects
    
     Changes to be committed:
    	modified:   server/gymLeader/gymLeaderModel.js
    	modified:   server/pokemon/pokemonModel.js

--- server/pokemon/pokemonModel.js
+++ server/pokemon/pokemonModel.js
@@ -1,7 +1,20 @@
 var mongoose = require('mongoose');
 
 var pokemonSchema = mongoose.Schema({
-  gameData:{}
+  name: String,
+  pokemonID: Number,
+  color: String,
+  description: String,
+  visible: {
+    type: Boolean,
+    default: true
+  },
+  alive: {
+    type: Boolean,
+    default: true
+  },
+  imageURL: String,
+  specs: {}
 });
 
 var pokemon = mongoose.model('pokemon', pokemonSchema);
