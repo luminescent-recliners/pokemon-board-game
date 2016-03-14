1ad5d80339bf114f8db626892d542fc359de124e
chououtside <chouming3@gmail.com>
2016-03-14 16:17:04 -0700

    Create Game Dashboard Factory and Get Player Options Method 	modified:   public/services.js

--- public/services.js
+++ public/services.js
@@ -20,4 +20,27 @@ angular.module('services', [])
     addStarterPokemon: addStarterPokemon
   };
   
+})
+.factory('gameDashboardFactory', function ($http) {
+  //Input: Receives dice roll, userId, and gameId
+  //Output: Retrieves an array of possible game spots to move
+  var getPlayerOptions = function (roll, gameId, userId) {
+    return $http({
+      method: 'GET',
+      url: '/api/games/playerOptions',
+      params: {
+        roll: roll,
+        gameId: gameId,
+        userId: userId
+      }
+    })
+    .then(function (resp) {
+      return resp.data;
+    });
+
+  };
+
+  return {
+    getPlayerOptions: getPlayerOptions
+  };
 });
