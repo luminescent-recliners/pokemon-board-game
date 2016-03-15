c144da1f63a304e050009e94fac3f2b066a69ff2
hiteshlala <hiteshlala@gmail.com>
2016-03-15 07:44:37 -0700

    Adds First Version of Board Spots Path Player
     On branch board-with-all-spots
     Changes to be committed:
    	modified:   public/board/board.html
    	modified:   public/board/board.js
    	modified:   public/board/boarddata.js
    	modified:   server/game/gameController.js
    	modified:   server/routes.js

--- public/board/board.js
+++ public/board/board.js
@@ -26,243 +26,165 @@ app.controller('boardController', function($scope, gameDashboardFactory, boardFa
   };
   
  // boardData will have to be preprocessed to be an array or change how
- // we store it.  Might be easier to just store as an array
- $scope.boardData = gameBoard;
- $scope.pathData = path;
+ // we store it.  
+ $scope.boardData = boardFactory.createBoardArray(gameBoardData);
+ $scope.pathData = boardFactory.createPath($scope.boardData);
+ $scope.pathString = boardFactory.createPathString($scope.pathData);
+
  $scope.playerPosition = 3;
  $scope.playerList = [];
  $scope.turn = 'player name or player index number';
- $scope.playerName = 'get after log in get this for board view?'
+ $scope.playerName = 'get after log in get this for board view?';
 
-  $scope.input;
+  $scope.input ='';
   $scope.inputValue = function($event) {
     if($event.which === 13) {
-      // $scope.boardData[3] = $scope.input;
-      // $scope.doublea[1][3] = $scope.input;
-      // $scope.boardData[3]['colorOfSpot'] = $scope.input;
-      // $scope.boardData[0].users.push($scope.input);
       $scope.playerPosition = $scope.input;
       $scope.input = '';
     }
   };
 
-  $scope.input2;
-  $scope.testDirective = function(data) {
-    console.log('testDirective', data, $scope.pathData);
-    // if($event.which === 13) {
-    //   console.log('test directive',$scope.input2);
-    //   // $scope.playerPosition = $scope.input2;
-    //   $scope.input2 = "";
-    // }
-  };
-
-  // $scope.init();
-});
-
-// was first iteration using an example given
-app.directive('drawBoard',function() {
-  var drawd3 = function(scope, element, attrs){
-    var chart = d3.select(element[0]);
-      chart.selectAll('*').remove();
-      chart.append("div").attr("class", "chart")
-       .selectAll('div')
-       .data(scope.data).enter().append("div")
-       .transition().ease("elastic")
-       .style("width", function(d) { return d + "%"; })
-       .text(function(d) { return d + "%"; });
-  };
-
-  var directiveObject = {
-    restrict: 'E',
-    replace: false,
-    scope: {data: '=boardData'},
-    link: function(scope, element, attrs) {
-      drawd3(scope, element, attrs);
-      scope.$watch('data', function(newValue, oldValue) {
-        if(newValue) {
-          drawd3(scope, element, attrs);
-          console.log("A data change", attrs);
-        }
-      },true);
-    }
-  };
-  return directiveObject;
-});
-
-// this was second iteration
-app.directive('drawNode', function() {
-
-  // this draws the node on the canvas
-  var drawNode = function(scope, element, attrs) {
-    // decide on dimensions of svg canvas and figure out
-    // multipliers
-    // have to figure out how to get his info from the
-    // document itself to allow resizing
-    var width = 1200;
-    var height= 800;
-    var wfactor = width/20;
-    var hfactor = height/4.3;
-    // select node on html where we will be adding board 
-    // using d3
-    var board = d3.select(element[0])
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height);
-
-    board.selectAll('.dontexistyet')
-      .append('circle')
-      .data(scope.data)
-      .enter()
-      .append('circle')
-      .transition().ease('elastic')
-      .attr('r', 15)
-      .attr('fill', function(d){return d.colorOfSpot;})
-      .attr('cx', function(d){return 50 + d.col * wfactor;})
-      .attr('cy', function(d){return 50 + d.row * hfactor;});
-  };
-
-  var directiveObject = {
-    restrict: 'E',
-    replace: false,
-    scope: {data: '=nodeData'},
-    link: function(scope, element, attrs) {
-      drawNode(scope, element, attrs);
-      console.log('from link function scope=',scope.data);
-
-      scope.$watch('data', function(newValue, oldValue) {
-        if(newValue) {
-          drawNode(scope, element, attrs);
-          console.log("A data change", newValue, oldValue);
-        }
-      }, true);
-
-    }
-  };
-  return directiveObject;
-
 });
 
 app.factory('boardFactory', function($http) {
 
+  var createBoardArray = function(board) {
+    var xRatio = 6; // total length 120
+    var yRatio = 12; // total length 60
+    for(var i = 1; i <= 73; i++) {
+      if(i <= 18) {
+        board[i].row = yRatio;
+        board[i].col = xRatio * i;
+
+      } else if(i <= 37) {
+        board[i].row = 2 * yRatio;
+        board[i].col = (xRatio * 19) - (i % 19) * xRatio;
+
+      } else if(i <= 56) {
+        board[i].row = 3 * yRatio;
+        board[i].col = xRatio * (i % 19);
+
+      } else {
+        board[i].row = 4 * yRatio;
+        board[i].col = (xRatio * 19) - (i % 18) * xRatio;
+      }
+    }
+    var boardArray = [];
+    for(key in board) {
+      boardArray.push(board[key]);
+    }
+    return boardArray;
+  };
+
+  var createPath = function(boardArray) {
+    var xscale = 10;
+    var yscale = 10;
+    var path = boardArray.map(function(item, index) {
+      var x = item.col * xscale;
+      var y = item.row * yscale;
+      return [x, y];
+    });
+    return path;
+  };
+
+  var createPathString = function(coordinates) {
+      var coords = coordinates.map(function(xypair){
+        return xypair[0] + ',' + xypair[1];
+      });
+      coords = coords.join('L');
+      coords = 'M' + coords;
+      return coords;
+  };
+
   // assume that this function retuns only the
-  // game board
+  // game board could not get data extracted
   var getBoard = function() {
-    return fakeboard;
-      // return $http({
-    //   method: 'GET',
-    //   url: '/games/' + gameId
-    // })
-    // .then(function(resp) {
-    //   return resp.data;
-    // });
-
-  };
-
-  // draw player on board
-  var drawPlayer = function() {
-
-  };
-
-  // move player
-  var movePlayer = function() {
-
+    return $http({
+      method: 'GET',
+      url: '/api/games/getBoard/?gameId=1',
+      // data: {
+      //   gameId: gameId
+      // }
+    })
+    .then(function(resp) {
+      console.log(resp.data);
+      return resp.data;
+    });
   };
 
   return {
-    getBoard: getBoard
+    getBoard: getBoard,
+    createPath: createPath,
+    createBoardArray: createBoardArray,
+    createPathString: createPathString
   };
 });
 
-// third iteration using template
-// most functionality we need done in this example
 app.directive('drawSpot', function() {
-  var directiveObject = {
+  return {
     templateNamespace: 'svg',
     restrict: 'E',
     replace: true,
     scope: {data: '=nodeData'},
     template: function() {
-        return '<circle ng-attr-cx="{{50+data.col*70}}" ng-attr-cy="{{50+data.row*50}}"'+
+        return '<circle ng-attr-cx="{{data.col*10}}" ng-attr-cy="{{data.row*10}}"'+
           ' r=12 fill={{data.colorOfSpot}} data-node={{data}}/>';
     },
 
     link: function(scope, element1, attrs) {
-
+      
+      // both options below show up in the dom tree but does not render the first 
+      // with the directive tag instead of the text tag.
       if(scope.data.users.length > 0) {
+        // for (var i = 0; i < scope.data.users.length; i++) {
+        //   var newTag = angular.element('<write-name data='+scope.data.users[i]+'>'+scope.data.users[i]+'</write-name>');
+        //   angular.element(element1[0]).after(newTag);
+        // }
         for (var i = 0; i < scope.data.users.length; i++) {
-          var newTag = angular.element('<write-name data='+scope.data.users[i]+'>'+scope.data.users[i]+'</write-name>');
+          var x = Number(attrs.cx) + 15;
+          var y = Number(attrs.cy) + 15;
+          var newTag = angular.element('<text data="'+scope.data.users[i]+
+            '" fill="#00BCD4" ng-attr-x="' + x + '"ng-attr-y="' + y +
+            '">' + scope.data.users[i]+ '</text>');
           angular.element(element1[0]).after(newTag);
         }
-
-
-
-        // var domElementToAdd = '<text xmlns="http://www.w3.org/2000/svg" ng-attr-x="300" ng-attr-y="120" fill="red">angular append</text>';
-
-        // // '<svg xmlns="http://www.w3.org/2000/svg"><text fill="green" x="4" y="20" font-weight="bolder" font-size="2" font-family="Arial">89</text></svg>',
-
-        // var textTag = angular.element(domElementToAdd);
-        // console.log('have players textTag', textTag);
-        // angular.element(element1[0]).after(textTag);
-
-
-        // $(element1[0]).empty();
-        // var newText = document.createElementNS('http://www.w3.org/2000/svg','text');
-        // newText.setAttribute('id','line2');
-        // newText.setAttribute('ng-attr-x','300');
-        // newText.setAttribute('ng-attr-y','120');
-        // newText.setAttribute('fill','red');
-        // newText.setAttribute('value','say something different');
-
-        // var something = $(element1[0]).after(newText);
-          // .data(scope.data.users).enter().append('text')
-          // .text(scope.data.users[i]);
-
-      //   // <text x="0" y="15" fill="red">I love SVG!</text>
-      // console.log("from if in link", something);
       }
 
       element1.on('mouseenter', function(event) {
-        console.log('from mousenter scope.data:', scope.data.colorOfSpot );
-        console.log('from mousenter attrs.fill:', attrs.fill );
-        // figure out angular pop up box populate with data
-        // 
-        // 
-        element1.attr('fill', 'pink');
+        // do other stuff
+        element1.attr('fill', 'orange');
       });
 
       element1.on('mouseleave', function(event) {
         // do other stuff
         element1.attr('fill', scope.data.colorOfSpot);
       });
-
-      // since populating through angular do not need to set watch
-      // scope.$watch('data', function(newValue, oldValue) {
-      //   if(newValue) {
-      //     console.log("A data change", attrs);
-      //   }
-      // },true);
-
     }
   };
-  return directiveObject;
-
 });
 
+
+// right now this one is hard coded cant figure out why it will
+// not read from the input data
 app.directive('drawPath', function() {
-  var directiveObject = {
+  return {
     templateNamespace: 'svg',
     restrict: 'E',
     replace: true,
-    scope: {path: '=pathArray'},
+    scope: {path1: '=pathString'},
     template: function(){
-      var coords = path.map(function(xypair){
-        return xypair[0] + ',' + xypair[1];
-      });
-      var out = '<path d="M' + coords.join('L') + '" stroke="#795548" stroke-width=3 fill="none"></path>';
-      return out;
+      // var coords = path.map(function(xypair){
+      //   return xypair[0] + ',' + xypair[1];
+      // });
+      // var out = '<path d="M' + coords.join('L') + '" stroke="#795548" stroke-width=3 fill="none"></path>';
+      // return out;
+      // return '<path d="{{path1}}" stroke="#795548" stroke-width=3 fill="none"></path>';
+      // return '<path></path>';
+
+      return '<path d="M60,120L120,120L180,120L240,120L300,120L360,120L420,120L480,120L540,120L600,120L660,120L720,120L780,120L840,120L900,120L960,120L1020,120L1080,120L1140,240L1080,240L1020,240L960,240L900,240L840,240L780,240L720,240L660,240L600,240L540,240L480,240L420,240L360,240L300,240L240,240L180,240L120,240L60,240L0,360L60,360L120,360L180,360L240,360L300,360L360,360L420,360L480,360L540,360L600,360L660,360L720,360L780,360L840,360L900,360L960,360L1020,360L1080,360L960,480L900,480L840,480L780,480L720,480L660,480L600,480L540,480L480,480L420,480L360,480L300,480L240,480L180,480L120,480L1140,480L1080,480" stroke="#795548" stroke-width=3 fill="none"></path>';
+
     }
   };
-  return directiveObject;
 });
 
 app.directive('writeName', function() {
@@ -287,8 +209,8 @@ app.directive('drawPlayer', function($animate) {
       board: '=board'
     },
     template: function() {
-      var x = '{{50+board[position].col*70}}';
-      var y = '{{70+board[position].row*50}}';
+      var x = '{{board[position].col * 10}}';
+      var y = '{{board[position].row * 10}}';
       var r = '18';
       var fillOpacity = '.9';
       var fillColor = 'beige';
@@ -313,24 +235,4 @@ app.directive('drawPlayer', function($animate) {
   };
 });
 
-// for testing if a directive can change scope variables
-app.directive('testEntry', function() {
-  return {
-    // templateNamespace: 'svg',
-    restrict: 'E',
-    replace: true,
-    scope: {
-      // player: '=player',
-      // position: '=position',
-      data: '=input2',
-      eventHandler: '&ngClick'
-    },
 
-    template: function(scope, two, three, four) {
-      console.log('from directive', scope, two, three, four);
-      return '<div>'+'<input type="text"/>'+
-        '<input type="button" data-ng-click="eventHandler(data)">hello</input>'+
-        '</div>';
-    }
-  };
-});
