4c5eff9a39279c7ceea673c87110e69cce449293
chououtside <chouming3@gmail.com>
2016-03-16 12:21:21 -0700

    Add Route to Move Player

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,7 @@ var gameController = require('./game/gameController.js');
 
 
 router.put('/api/games/addPokemon', gameController.addPokemon);
+router.put('api/games/user/movePlayer', gameController.movePlayer);
 router.post('/api/games/addGame', gameController.addGame);
 router.get('/api/games/getGames', gameController.getGames);
 router.get('/api/games/name', gameController.findName);
