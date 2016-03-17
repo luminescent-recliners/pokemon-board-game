3074c5d0d548383561f76f7d124acbe95ada0385
Arthi <aarti156@gmail.com>
2016-03-17 15:12:33 -0700

    Hides the start game button in lobby view if you are not the game creator
    modified:   public/lobby/lobby.html
    modified:   public/lobby/lobby.js
    modified:   server/game/gameController.js

--- public/lobby/lobby.js
+++ public/lobby/lobby.js
@@ -4,13 +4,16 @@ angular.module('pokemon.lobby', [])
   $scope.lobbytest = "Welcome to the Lobby!";
   $scope.gamename;
   $scope.gameCreator;
+  $scope.myGameCreator = false;
 
   // Post Dev values
   // $scope.gameId = $window.loaclStorage.getItem('pokemon.gameId');
+  // $scope.userId = $window.localStorage.getItem('pokemon.userId');
   // $scope.displayName = $window.localStorage.getItem('pokemon.displayName');
 
   //Dev values
   $scope.gameId = 1;
+  $scope.userId = "Facebook123";
   $scope.displayName = "Bob";
   
   //Todo: $scope.users has to be got from database in user table
@@ -24,6 +27,10 @@ angular.module('pokemon.lobby', [])
       .then(function (resp) {
         $scope.gameName = resp.gameName;
         $scope.gameCreator = resp.gameCreator;
+        $scope.gameCreatorName = resp.creatorName;
+        if($scope.gameCreator !== $scope.userId) {
+          $scope.myGameCreator = true 
+        } 
       }).catch(function (error) {
         console.error(error);
       });
