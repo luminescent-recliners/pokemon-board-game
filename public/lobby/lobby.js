67d0069b9384a9f151aa4cc73e941089c165ee18
Arthi <aarti156@gmail.com>
2016-03-17 13:24:33 -0700

    Refactors users array to user schema in database
    Fixes the userId to be facebookId
    
    modified:   public/lobby/lobby.html
    modified:   public/lobby/lobby.js

--- public/lobby/lobby.js
+++ public/lobby/lobby.js
@@ -1,11 +1,13 @@
 angular.module('pokemon.lobby', [])
 .controller('lobbyController', function ($scope, $location, gameFactory) {
   $scope.lobbytest = "Welcome to the Lobby!";
-  $scope.userId = "arthicuno";
+  $scope.userId = "Facebook123";
   $scope.gameId = 1;
   $scope.gamename;
+  //Todo: $scope.users has to be got from database in user table
   $scope.users = [
-    {facebookId: "Facebook123", userId: "arthicuno"}, {facebookId: "Facebook456", userId: "choumander"}
+    {facebookId: "Facebook123", displayName: "arthicuno"}, 
+    {facebookId: "Facebook456", displayName: "choumander"}
   ];
 
   $scope.initialize = function () {
