489e7fa40492ea1e6b95c15300f2dabbf6ddf9e9
hiteshlala <hiteshlala@gmail.com>
2016-03-19 15:01:14 -0700

    starts sockets

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ mongoose.connect('mongodb://localhost/pokemon');
 var router = require('./routes.js');
 var app = express();
 
+
+// for sockets
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -15,7 +20,22 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(router);
 app.use(express.static(__dirname + '/../public'));
 
-app.listen(port);
-console.log('Server listening on..', port);
+// for sockets
+var conectioncounter = 0;
+io.on('connection', function(socket){
+  console.log('a user connected', conectioncounter++, socket);
+  socket.on('disconnect', function(something) {
+    conectioncounter--;
+    console.log('a user disconnected', conectioncounter, something);
+  });
+});
+
+// app.listen(port);
+// console.log('Server listening on..', port);
+
+// for sockets
+http.listen(port, function() {
+  console.log('Server listening on..', port);
+});
 
 module.exports = app;
