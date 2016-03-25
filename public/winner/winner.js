5392df267e2e917c3073c920e4741185bf128402
Arthi <aarti156@gmail.com>
2016-03-25 11:47:39 -0700

    Implements sockets to get the winner on initialization of winner page
    Changes to be committed:
    modified:   public/winner/winner.js
    modified:   server/server.js

--- public/winner/winner.js
+++ public/winner/winner.js
@@ -1,4 +1,15 @@
 angular.module('pokemon.winner', [])
-.controller('winnerController', function ($scope) {
+.controller('winnerController', function ($scope, $window, pokemonSocket) {
   $scope.winnertest = "someone won the game!";
+
+  $scope.gameId = $window.localStorage.getItem('pokemon.gameId');
+
+  var winnerInit = function () {
+    pokemonSocket.emit('get winner', { gameId: $scope.gameId });
+    pokemonSocket.on('display winner', function (data) {
+      $scope.winner = data.winner;
+    })
+  };
+
+  winnerInit();
 });
