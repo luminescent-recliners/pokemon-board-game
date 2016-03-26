5b1a102787ba518f58ea52a95ec5dc0abe2754ee
Linda Zou <linda.ye.zou@gmail.com>
2016-03-25 18:22:19 -0700

    Establishes angular-audio dependencies
     Changes to be committed:
    	modified:   app.js
    	modified:   bower.json
    	modified:   home/home.html
    	modified:   home/home.js
            modified:   index.html

--- public/app.js
+++ public/app.js
@@ -13,7 +13,8 @@ angular.module('pokemon', [
   'pokemon.auth',
   'pokemon.winner',
   'ngCookies',
-  'ngRoute'
+  'ngRoute',
+  'ngAudio'
 ])
 .config(function($routeProvider) {
   $routeProvider
