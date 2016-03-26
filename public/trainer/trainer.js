54623cc38a11a64b1393efe52ad814b639ddeee5
chououtside <chouming3@gmail.com>
2016-03-26 15:22:16 -0700

    Add Trainer View And Controller
    Created new view for when player lands on a spot and chooses to exercise the trainer action
    
     Changes to be committed:
    	modified:   public/app.js
    	modified:   public/index.html
    	new file:   public/trainer/trainer.html
    	new file:   public/trainer/trainer.js

---
+++ public/trainer/trainer.js
@@ -0,0 +1,68 @@
+angular.module('pokemon.trainer', [])
+.controller('trainerController', function ($scope, gameFactory, $window, $location, pokemonSocket) {
+
+  // Post Dev values
+  $scope.gameId = $window.localStorage.getItem('pokemon.gameId');
+  $scope.facebookId = $window.localStorage.getItem('pokemon.facebookId');
+  $scope.playerName = $window.localStorage.getItem('pokemon.playerName');
+
+  $scope.currentTurnPlayerName;
+  $scope.currentTurnPlayerId;
+  
+  $scope.updateTurn = function () {
+    gameFactory.updateTurn($scope.gameId, 'boardView')
+      .then(function (resp) {
+        pokemonSocket.emit('emit users back to board', {gameId: $scope.gameId});
+        $location.path('/board');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  pokemonSocket.on('redirect back to board', function() {
+    $location.path('/board');
+  });
+
+  var initialize = function () {
+    gameFactory.getGameTurn($scope.gameId)
+      .then(function (resp) {
+        $scope.currentTurnPlayerName = resp.playerName;
+        $scope.currentTurnPlayerId = resp.facebookId;
+      });
+  };
+
+  var confirmCurrentPage = function() {
+    gameFactory.getCurrentPage($scope.gameId)
+      .then(function(currentPage){
+        if (currentPage === 'trainerView') {
+          initialize();
+        }else{
+          switch (currentPage) {
+            case 'starterView':
+              $location.path('/starter');
+              break;
+            case 'cityView':
+              $location.path('/city');
+              break;
+            case 'boardView':
+              $location.path('/board');
+              break;
+            case 'captureView':
+              $location.path('/capture');
+              break;
+            case 'eventView':
+              $location.path('/event');
+              break;
+          }
+        }
+      });
+  };
+
+  confirmCurrentPage();
+
+  // setTimeout(function(){ 
+  //   pokemonSocket.emit('emit users back to board', {gameId: $scope.gameId});
+  // }, 3000);
+
+});
