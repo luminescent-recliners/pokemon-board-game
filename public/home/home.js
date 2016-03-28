a9c762e46def2e94ce2527e74b515e92e531ab9c
Arthi <aarti156@gmail.com>
2016-03-28 11:54:31 -0700

    Fixes merge conflict in home view join lobby function
    Changes to be committed:
    modified:   public/home/home.js

--- public/home/home.js
+++ public/home/home.js
@@ -62,27 +62,37 @@ angular.module('pokemon.home', [])
     }
   };
 
-  $scope.joinLobby = function(id) {
-    gameFactory.requestLobbyEntry(id)
+  $scope.joinLobby = function(id, gameStarted) {
+    if(gameStarted === false) {
+      gameFactory.requestLobbyEntry(id)
       .then(function (resp) {
         if(resp.requestAccepted) {
           $window.localStorage.setItem('pokemon.gameId', id);
           $location.path('/lobby');
-
           pokemonSocket.emit('joinLobby', {
             gameId: id,
             user: {
               playerName: $scope.playerName,
               facebookId: $scope.facebookId
             }
-          }); 
+          });
         } else {
           alert("Sorry, This game already has 6 players! Pls select another game.");
         }
       }) 
       .catch(function (error) {
         console.error(error);
-      });
+      });  
+    } else {
+      $location.path('/resumelobby');
+      pokemonSocket.emit('join resume lobby', {
+        gameId: id,
+        user: {
+          playerName: $scope.playerName,
+          facebookId: $scope.facebookId
+        }
+      }); 
+    } 
   };
 
   var userGames = function() {
