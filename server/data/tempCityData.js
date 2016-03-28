c8d35516fac59cc11508c2d9e83ed35ade2dd8bd
Linda Zou <linda.ye.zou@gmail.com>
2016-03-28 11:18:29 -0700

    Adds proper gifs and descriptions and refactors views to match
     Changes to be committed:
    	modified:   city/city.html
    	modified:   event/event.html
    	modified:   ../server/data/tempCityData.js
    	modified:   ../server/data/tempEventData.js

--- server/data/tempCityData.js
+++ server/data/tempCityData.js
@@ -1,15 +1,27 @@
 module.exports = 
 [
 	{
-	description: "CITY - Pokemon information and stuff ONE (1)",
-	cityURL: "https://media.giphy.com/media/11NlXRhFSZl4w8/giphy.gif"
+	description: "Time to laugh and rest with your friends!",
+	cityURL: "http://25.media.tumblr.com/tumblr_lnbco3N0Bg1ql004jo1_500.gif"
 	},
 	{
-	description: "CITY - Pokemon information and stuff TWO (2)",
-	cityURL: "http://37.media.tumblr.com/a9fdba0f60320a45fe7a3b212fe84cca/tumblr_n8xfipGOgu1r8zm42o1_500.gif",
+	description: "Be sure to visit Nurse Joy in the Pokemon Center!",
+	cityURL: "https://secure.static.tumblr.com/1a35bfb63461abf0b9e37880a96ac303/dgaksuh/PvGnm60nx/tumblr_static_tumblr_static__640.gif",
 	},
 	{
-	description: "CITY - Pokemon information and stuff THREE (3)",
+	description: "Satisfy all of your food cravings before hitting the road again!",
 	cityURL: "http://49.media.tumblr.com/tumblr_luq5wx0fq71qm8s9no1_400.gif"
+	},
+	{
+	description: "Get stocked up on food and supplies at the shop!",
+	cityURL: "hhttp://i3.kym-cdn.com/photos/images/newsfeed/000/256/385/152.gif"
+	},
+	{
+	description: "Recharge your Pokedex and switch out your party!",
+	cityURL: "http://24.media.tumblr.com/tumblr_lpkwzi4rXB1qi3civo1_500.gif"
+	},
+	{
+	description: "Its good to train but dont forget to rest",
+	cityURL: "http://geekandsundry.com/wp-content/uploads/2016/03/pokemonfit.gif"
 	}
 ]
