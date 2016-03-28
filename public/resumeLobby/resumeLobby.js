ed9b860ed83a75a2a879644dc2e6cebb22f5490b
Arthi <aarti156@gmail.com>
2016-03-27 18:25:55 -0700

    Adds socket to update list of users entering resume game lobby Changes to be committed: modified:   public/resumeLobby/resumeLobby.html modified:   public/resumeLobby/resumeLobby.js

--- public/resumeLobby/resumeLobby.js
+++ public/resumeLobby/resumeLobby.js
@@ -1,12 +1,23 @@
 angular.module('pokemon.resumelobby', [])
-.controller('resumeLobbyController', function ($scope, $window, $location, gameFactory) {
+.controller('resumeLobbyController', function ($scope, $window, $location, gameFactory, pokemonSocket) {
   $scope.resumelobbytest = " welcome to the resume game Lobby!";
 
   $scope.gameId = $window.localStorage.getItem('pokemon.gameId');
   $scope.facebookId = $window.localStorage.getItem('pokemon.facebookId');
   $scope.playerName = $window.localStorage.getItem('pokemon.playerName');
+  $scope.users = [];
 
-  $scope.isCreator = false;
+  pokemonSocket.on('join resume lobby', function (data) {
+    $scope.users.push(data);
+  });
+
+  pokemonSocket.on('results for check users', function (data) {
+    if(data.result) {
+      $scope.allusersavailable = true;
+    } else {
+      $scope.allusersavailable = false;
+    }
+  });
 
   var initialize = function (gameId) {
     gameFactory.lobbyInit($scope.gameId)
@@ -23,6 +34,7 @@ angular.module('pokemon.resumelobby', [])
   initialize($scope.gameId);
 
   $scope.getBoardView = function () {
+    pokemonSocket.emit('check if all users are here', { gameId: $scope.gameId, NumberOfUsers: $scope.users.length });
     $location.path('/board');
   };
 
