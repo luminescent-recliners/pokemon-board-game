09f20028a5662b353bcb34af236452d4cab55978
Linda Zou <linda.ye.zou@gmail.com>
2016-03-28 11:18:29 -0700

    Adds proper gifs and descriptions and refactors views to match
     Changes to be committed:
    	modified:   city/city.html
    	modified:   event/event.html
    	modified:   ../server/data/tempCityData.js
    	modified:   ../server/data/tempEventData.js

--- server/data/tempEventData.js
+++ server/data/tempEventData.js
@@ -1,15 +1,27 @@
 module.exports = 
 [
 	{
-	description: "EVENT - Pokemon information and stuff ONE (1)",
+	description: "Dance Party!!",
 	eventURL: "https://media.giphy.com/media/11NlXRhFSZl4w8/giphy.gif"
 	},
 	{
-	description: "EVENT - Pokemon information and stuff TWO (2)",
-	eventURL: "http://37.media.tumblr.com/a9fdba0f60320a45fe7a3b212fe84cca/tumblr_n8xfipGOgu1r8zm42o1_500.gif",
+	description: "Pikachu Dance Solo!",
+	eventURL: "http://thieunien.vn/files/s-17/2015/1115/5648a9a3-818eb1a0b3.gif",
 	},
 	{
-	description: "EVENT - Pokemon information and stuff THREE (3)",
-	eventURL: "http://49.media.tumblr.com/tumblr_luq5wx0fq71qm8s9no1_400.gif"
+	description: "Celebration!",
+	eventURL: "http://i.imgur.com/NdhDCrb.gif"
+	},
+	{
+	description: "Boxing Match!",
+	eventURL: "http://media.tumblr.com/tumblr_lrlh7ro3Nr1qect1t.gif"
+	},
+	{
+	description: "Its time to stop and play in the puddles!",
+	eventURL: "http://37.media.tumblr.com/a9fdba0f60320a45fe7a3b212fe84cca/tumblr_n8xfipGOgu1r8zm42o1_500.gif"
+	},
+	{
+	description: "Free piggy back rides!",
+	eventURL: "http://chimera.mixrmedia.com/wp-uploads/ningin/blog/2012/04/squirtle-pikachu.gif"
 	}
 ]
