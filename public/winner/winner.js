ab4a415efa630ff6e1ce948f4a2e16cef07fcede
Arthi <aarti156@gmail.com>
2016-03-30 15:04:32 -0700

    Creates logic to display circles with winner's pokemon
    implements loops to get the gifURL of winner's pokemons
    Changes to be committed:
    modified:   public/winner/winner.js

--- public/winner/winner.js
+++ public/winner/winner.js
@@ -2,13 +2,44 @@ angular.module('pokemon.winner', [])
 .controller('winnerController', function ($scope, $window, $location, userFactory, gameFactory, pokemonSocket) {
   $scope.gameId = $window.localStorage.getItem('pokemon.gameId');
 
+  $scope.circleData = [
+    {row: 225, col: 30, typeOfSpot: 'white'},
+    {row: 350, col: 150, typeOfSpot: 'pink'},
+    {row: 105, col: 150, typeOfSpot: 'blue'},
+    {row: 225, col: 270, typeOfSpot: 'green'},
+    {row: 165, col: 255, typeOfSpot: 'green'},
+    {row: 120, col: 210, typeOfSpot: 'blue'},
+    {row: 330, col: 90, typeOfSpot: 'pink'},
+    {row: 285, col: 50, typeOfSpot: 'pink'},
+    {row: 285, col: 250, typeOfSpot: 'green'},
+    {row: 330, col: 210, typeOfSpot: 'pink'},
+    {row: 120, col: 90, typeOfSpot: 'red'},
+    {row: 163, col: 42, typeOfSpot: 'red'}
+  ];
+
+  pokemonSocket.on('display winner', function (data) {
+    $scope.winner = data.winner;
+    $scope.party = data.winner.party;
+    for(var i = 1; i < $scope.circleData.length; i++) {
+      for(var j = 0; j < $scope.party.length; j++) {
+        if($scope.party[j].color === $scope.circleData[i].typeOfSpot) {
+          $scope.circleData[i].gifURL = $scope.party[j].gifURL;
+          $scope.party.splice(j, 1);
+          i++;
+        } else if( $scope.party[j].color === "starter") {
+          $scope.circleData[0].gifURL = $scope.party[j].gifURL;
+          $scope.party.splice(j ,1);
+          i++;
+        }
+      }
+    }
+
+  });
+
   var initialize = function () {
     userFactory.getUsers($scope.gameId)
       .then(function (resp) {
         pokemonSocket.emit('get winner', { gameId: $scope.gameId });
-        pokemonSocket.on('display winner', function (data) {
-          $scope.winner = data.winner;
-        });
         $scope.users = resp;
       })
       .catch(function (error) {
