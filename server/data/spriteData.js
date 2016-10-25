45c795916106dee4722a7beb3c5ef467b0e51f53
chououtside <chouming3@gmail.com>
2016-10-25 13:54:00 -0400

    Update trainer sprites

--- server/data/spriteData.js
+++ server/data/spriteData.js
@@ -4,41 +4,41 @@ module.exports = [
     spriteId: 1,
     type: 'trainer',
     description: 'A description of Sprite still to come.',
-    imageURL: 'http://sprites.pokecheck.org/t/187.gif'
+    imageURL: 'http://vignette4.wikia.nocookie.net/pokemon/images/4/4e/Janine%28HGSS%29Sprite.gif/revision/latest?cb=20100311133924'
   },
   {
     name: 'Male Trainer',
     spriteId: 2,
     type: 'trainer',
     description: 'A description of Sprite still to come.',
-    imageURL: 'http://sprites.pokecheck.org/t/137.gif'
+    imageURL: 'http://vignette4.wikia.nocookie.net/pokemon/images/5/54/Rival_Blue.gif/revision/latest?cb=20110628163247'
   },
   {
     name: 'Female Trainer',
     spriteId: 3,
     type: 'trainer',
     description: 'A description of Sprite still to come.',
-    imageURL: 'http://sprites.pokecheck.org/t/123.gif'
+    imageURL: 'http://vignette3.wikia.nocookie.net/pokemon/images/e/e1/RivalPlatinum.gif/revision/latest?cb=20090903051836'
   },
   {
     name:  'Female Trainer',
     spriteId: 4,
     type: 'trainer',
     description: 'A description of Sprite still to come.',
-    imageURL: 'http://sprites.pokecheck.org/t/003.gif'
+    imageURL: 'http://67.media.tumblr.com/8ac2ce7ad86e78b77df5e69938b15508/tumblr_n4gtn62q5x1smedslo7_100.gif'
   },
   {
     name: 'Male Trainer',
     spriteId: 5,
     type: 'trainer',
     description: 'A description of Sprite still to come.',
-    imageURL: 'http://sprites.pokecheck.org/t/049.gif'
+    imageURL: 'http://orig15.deviantart.net/36af/f/2010/173/7/5/crystal_trainer_sprite_by_kiraga_neko.gif'
   },
   {
     name: 'Female Trainer',
     spriteId: 6,
     type: 'trainer',
     description: 'A description of Sprite still to come.',
-    imageURL: 'http://sprites.pokecheck.org/t/048.gif'
+    imageURL: 'http://orig02.deviantart.net/5c62/f/2010/173/3/2/ash_trainer_animated_by_j3010.gif'
   }
 ];
